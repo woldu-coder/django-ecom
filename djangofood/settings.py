924e5f290140dc2b89aff2dcf2eb13dde1251b22
woldu-coder <yeroo.sime@gmail.com>
2023-04-12 10:19:49 +0300

    req

--- djangofood/settings.py
+++ djangofood/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-dtu27rqmqm73#6dk$w%rrnnqoij%&%e2pf8sm4fjs#2ih8-#8$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
